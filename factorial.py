98b521cac152f4cb1fbdc1ec557ade9c25dd6030
ankita080208 <52229554+ankita080208@users.noreply.github.com>
2020-10-26 20:28:15 +0530

    Update factorial.py
    finding the factorial with less complication in code and also added line for finding the factorial of 0.

--- factorial.py
+++ factorial.py
@@ -1,16 +1,17 @@
-# hello
-import sys
-
-
+#defining the function "recur_factorial()" which will calculate teh factorial of given input of int datatype by user.
 def recur_factorial(n):
     """Function to return the factorial
     of a number using recursion"""
+#below condition will be true then the output will be number i.e, 1.
+    if n == 0:
+        return 1.
+#below condition will be true then the output will be number i.e, 1.
     if n == 1:
         return n
+#otherwise if the number is greater than 1 then the following recursive formula will be used.
     else:
-        return n*recur_factorial(n-1)
-
-
-if __name__ == '__main__':
-    arg = sys.argv
-    print(recur_factorial(int(arg[1])))
+        return n*recur_factorial(n-1)   
+#storing the given input by user in a variable.
+ n=int(input("enter the number = "))
+ print(recur_factorial(n))
+#end of code.
